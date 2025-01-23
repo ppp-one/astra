bfa43a08a9c12c873dcdd2457e9ed2b02339406b
Peter Pihlmann Pedersen <peter@ppp.one>
2025-01-23 13:14:41 +0100

    popup bug removed

--- src/astra/frontend/js/weather.js
+++ src/astra/frontend/js/weather.js
@@ -6,7 +6,9 @@
 // - dynamic update of the plots
 // - format tables
 
+let refreshInterval;
 
+// Color palette for weather parameters
 function color_palette(parameter) {
     const colorMap = {
         CloudCover: "rgba(180, 180, 180, 0.5)", // Light gray, slightly more opaque for visibility
@@ -75,7 +77,8 @@ function addUnits(parameter, weather_safety_limits) {
     }
 }
 
-function plotWeather(data, observatory, update, popup) {
+// Function to plot weather data
+function plotWeather(data, observatory, update) {
 
     console.log("Plotting weather data for ", observatory);
 
@@ -146,7 +149,7 @@ function plotWeather(data, observatory, update, popup) {
 
 
     document.getElementById(`weather-latest-${observatory}`).innerHTML = `
-        <table class="table-auto w-full proportional-nums font-variant-numeric rounded-lg bg-gray-600/20" id="weather-table-${observatory}">
+        <table class="table-auto w-full proportional-nums font-variant-numeric rounded-lg bg-gray-600/20" id="weather-table-${observatory}" title="Last refreshed: ${new Date().toISOString().slice(0, 19).replace("T", " ")}">
         <thead>
             <tr class="border-b-2 border-b-slate-500">
                 <td class="py-1.5 px-3" style="text-align: left;">Parameter</td>
@@ -386,42 +389,6 @@ function plotWeather(data, observatory, update, popup) {
             }
         });
     };
-
-    let table = document.getElementById(`weather-table-${observatory}`)
-    let refreshTime = new Date().toISOString().slice(0, 19).replace("T", " ");
-    table.addEventListener('mouseover', (e) => {
-        popup.classList.remove('hidden');
-        popup.classList.add('block');
-
-        // If less than 10 lines, just set the text
-        popup.innerHTML = "Last refreshed: " + refreshTime;
-
-        movePopup(e);
-    });
-
-    table.addEventListener('mousemove', (e) => {
-        movePopup(e);
-    });
-
-    table.addEventListener('mouseout', () => {
-        popup.classList.remove('block');
-        popup.classList.add('hidden');
-
-    });
-
-    table.addEventListener('click', (e) => {
-        // toggle
-        console.log("clicked")
-        if (popup.classList.contains('hidden')) {
-            popup.classList.remove('hidden');
-            popup.classList.add('block');
-        } else {
-            popup.classList.remove('block');
-            popup.classList.add('hidden');
-        }
-    });
-
-
 
     createWeatherPlots(
         weather_data,
